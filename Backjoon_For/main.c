c2d55013537293b0cfbcd436bd4da32d20e09f6a
KimBoris <89508080+KimBoris@users.noreply.github.com>
2022-01-04 19:18:33 +0900

    백준 for문
    2439다시 풀기 ( 문제 이해 잘못했음.. 바보.)

---
+++ Backjoon_For/main.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+
+int main()
+{
+	//백준 for문
+
+	//2739
+	//int i = 0, j = 0;
+	//scanf_s("%d", &i);
+
+	//while (0 < i && i < 10)
+	//{
+	//	for (j = 1; j <= 9; j++)
+	//	{
+	//		printf("%d * %d = %d\n", i, j, i * j);
+	//	}
+	//	return;
+	//}
+
+
+	//10950
+	//int A = 0;
+	//int B = 0;
+	//int i = 0;
+	//int T = 0;
+	//scanf_s("%d", &T);
+	//for (i = 0; i < T; i++)
+	//{
+	//	scanf_s("%d %d", &A, &B);
+	//	printf("%d\n", A + B);
+	//}
+
+//8393
+	//int n= 0;
+	//int j = 0;
+	//scanf_s("%d", &n);
+	//
+	//for (int i = 1; i <= n; i++)
+	//{
+	//	j += i;
+	//}
+
+	//printf("%d", j);
+
+
+	//15552
+	/*int A = 0;
+	int B = 0;
+	int count = 0;
+
+	scanf_s("%d", &count);
+	for (int i = 0; i < count; i++)
+	{
+		scanf_s("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}*/
+
+	//2741
+	/*int A = 0;
+	scanf_s("%d", &A);
+	for (int i = 1; i <= A; i++)
+	{
+		printf("%d\n", i);
+	}*/
+
+	//2742
+	/*int A = 0;
+	scanf_s("%d", &A);
+
+	for (int i = A; i > 0; i--)
+	{
+		printf("%d\n", i);
+	}*/
+
+	//11021
+	//int A = 0;
+	//int B = 0;
+	//int count = 0;
+	//scanf_s("%d", &count);
+	//for (int i = 1; i <= count; i++)
+	//{
+	//	scanf_s("%d %d", &A, &B);
+	//	printf("Case #%d: %d\n", i, A + B);
+	//}
+
+
+
+	//11022
+	//int A = 0;
+	//int B = 0;
+	//int count = 0;
+
+	//scanf_s("%d", &count);
+	//for (int i = 1; i <= count; i++)
+	//{
+	//	scanf_s("%d %d", &A, &B);
+	//	printf("Case #%d : %d + %d = %d", i, A, B, A + B);
+	//}
+
+	//2438
+
+	/*int count = 0;
+	scanf_s("%d", &count);
+	for (int j = 1; j <= count; j++)
+	{
+		for (int i = 0; i < j; i++)
+		{
+			printf("*");
+		}
+		putchar('\n');
+	}*/
+
+
+
+
+
+
+//2439번 풀기
+	int A = 0;
+	int B = 0;
+	int count = 0;
+	scanf_s("%d", &count);
+
+	for (int j = 1; j < count; j++) {
+		for (int i = count - 1; i > count; i--)
+		{
+			putchar('@');
+		}
+		for (int k = 0; k < j; k++)
+		{
+			putchar('*');
+		}
+		putchar('\n');
+	}
+
+
+
+
+
+	return 0;
+
+
+
+
+
+
+}
